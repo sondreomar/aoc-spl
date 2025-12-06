15315e38b58a7cd1f46b50826ffe4b5fe9a6cb74
Sondre Arneberg <sondre.arneberg@bidbax.no>
2025-12-06 16:01:49 +0000

    feat: add inputaoc option to disable whitespace stripping

--- app/bin/inputaoc.py
+++ app/bin/inputaoc.py
@@ -5,13 +5,21 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
-from splunklib.searchcommands import Configuration, GeneratingCommand, dispatch
+from splunklib.searchcommands import Configuration, GeneratingCommand, Option, dispatch, validators
 
 INPUTS = Path("../aoc/inputs")
 
 
 @Configuration(type="reporting", streaming=False)
 class InputAOC(GeneratingCommand):
+    strip_whitespace = Option(
+        doc="""'
+        **Syntax:** **preserve_newline=***<integer>*
+        **Description:** Remove leading and trailing whitespace, default: True """,
+        validate=validators.Boolean(),
+        default=True,
+    )
+
     def generate(self):
         if not self.fieldnames:
             raise ValueError("No filename provided")
@@ -22,7 +30,9 @@ class InputAOC(GeneratingCommand):
             raise ValueError("File does not exist")
         with open(path) as f:
             for line in f:
-                yield {"_raw": line.strip()}
+                if self.strip_whitespace:
+                    line = line.strip()
+                yield {"_raw": line}
 
 
 dispatch(InputAOC, sys.argv, sys.stdin, sys.stdout, __name__)
