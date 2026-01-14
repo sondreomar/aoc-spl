66bded416a18216d145f2588b1708a380f986457
Sondre Arneberg <sondre.arneberg@bidbax.no>
2026-01-14 11:22:38 +0000

    feat: add p10k to devcontainer

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
+echo "Installing Powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $SPLUNK_HOME/powerlevel10k
+$SPLUNK_HOME/powerlevel10k/gitstatus/install
+cp .devcontainer/.p10k.zsh $SPLUNK_HOME/.p10k.zsh
+cp .devcontainer/.zshrc $SPLUNK_HOME/.zshrc
+
 echo "Setup GitHub CLI config..."
 mkdir -p $SPLUNK_HOME/.config/gh
 cp /tmp/gh-config-host/hosts.yml $SPLUNK_HOME/.config/gh/hosts.yml
