7d2f2eb94f6131f1a3f62a1e602c09b5e0ec4490
Sondre Arneberg <sondre.arneberg@bidbax.no>
2026-01-14 11:21:17 +0000

    feat: add github cli

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+echo "Setup GitHub CLI config..."
+mkdir -p $SPLUNK_HOME/.config/gh
+cp /tmp/gh-config-host/hosts.yml $SPLUNK_HOME/.config/gh/hosts.yml
+chmod 600 $SPLUNK_HOME/.config/gh/hosts.yml
+
 echo "Setup python environment..."
 uv sync --locked --python $SPLUNK_HOME/bin/python3
 uv pip install $(uv pip freeze | grep splunk-sdk) --target app/bin/lib
