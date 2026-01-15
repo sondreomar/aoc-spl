eb3ec1f284a6364af18cfcf16ab2dffff387dd7a
Sondre Arneberg <sondre.arneberg@bidbax.no>
2026-01-15 19:23:24 +0000

    formatting: better post_create output

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,21 +1,21 @@
 #!/bin/zsh
 
-echo "Installing Powerlevel10k..."
+printf "\nInstalling Powerlevel10k...\n"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $SPLUNK_HOME/powerlevel10k
 $SPLUNK_HOME/powerlevel10k/gitstatus/install
 cp .devcontainer/.p10k.zsh $SPLUNK_HOME/.p10k.zsh
 cp .devcontainer/.zshrc $SPLUNK_HOME/.zshrc
 
-echo "Setup GitHub CLI config..."
+printf "\nSetup GitHub CLI config...\n"
 mkdir -p $SPLUNK_HOME/.config/gh
 cp /tmp/gh-config-host/hosts.yml $SPLUNK_HOME/.config/gh/hosts.yml
 chmod 600 $SPLUNK_HOME/.config/gh/hosts.yml
 
-echo "Setup python environment..."
+printf "\nSetup python environment...\n"
 uv sync --locked --python $SPLUNK_HOME/bin/python3
 uv pip install $(uv pip freeze | grep splunk-sdk) --target app/bin/lib
 
-echo "Get splunk token..."
+printf "\nGet splunk token...\n"
 bash .devcontainer/get_splunk_token.sh
 
-echo "Setup complete!"
+printf "\nSetup complete!\n"
